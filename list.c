f20fa2a45e08a879dc512cc932e5ea0ddad58426
Replit user <>
2022-08-20 02:50:34 +0000

    new try

--- list.c
+++ list.c
@@ -87,7 +87,8 @@ void pushCurrent(List * list, void * data) {
   Node * nodo = createNode(data);
   nodo -> prev = list -> current;
   list -> head -> next = nodo;
-  nodo -> prev = list -> tail;
+  list -> tail -> prev = list -> tail;
+  list -> tail = nodo;
   // if(L->tail == NULL || L->tail->data != j){
        // err_msg("tail deberia apuntar al nuevo nodo");
         //return 0;
