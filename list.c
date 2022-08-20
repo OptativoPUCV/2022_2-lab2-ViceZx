3a27752c1160d765e5eb145d10f7aa845a50bdf5
Replit user <>
2022-08-20 03:40:04 +0000

    new try

--- list.c
+++ list.c
@@ -85,8 +85,8 @@ void pushBack(List * list, void * data) {
 void pushCurrent(List * list, void * data) {
   //Programe la función `void pushCurrent(List * list, void* data)`, la cual agrega un dato a continuación del nodo apuntado por `list->current`.
   Node * nodo = createNode(data);
-  nodo -> prev = list -> head;
-  list -> head -> next -> prev = nodo;
+  nodo -> prev = list -> current;
+  list -> current -> next -> prev = nodo;
   nodo -> prev = list -> current;
   list -> current -> next = nodo;
   nodo -> next = list -> tail;
